a32fdd637b59f1922c3b68b0d5323fecfde4a778
Philipp Bosch <hello@pb.io>
2011-12-17 01:07:40 +0100

    Test 03

--- javascripts/app.coffee
+++ javascripts/app.coffee
@@ -1,11 +1,22 @@
 $ ->
     nav = $('#slidenav')
+    touchSupport = ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch
+    startEvent = if touchSupport then 'touchstart' else 'mousedown'
+    endEvent = if touchSupport then 'touchend touchcancel touchleave' else 'mouseup mouseout'
+    moveEvent = if touchSupport then 'touchmove' else 'mousemove'
     
     log = (msg) ->
         $('#log').prepend("<li>#{msg}</li>")
     
-    nav.bind 'touchstart', (e) ->
-        $(this).addClass 'touched'
+    nav.bind startEvent, (e) ->
+        nav.addClass 'touched'
+        log e.type
     
-    nav.bind 'touchend touchcancel touchleave', (e) ->
-        $(this).removeClass 'touched'
+    nav.bind endEvent, (e) ->
+        return unless nav.is('.touched')
+        nav.removeClass 'touched'
+        log e.type
+    
+    nav.bind moveEvent, (e) ->
+        return unless nav.is('.touched')
+        log e.type
--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,36 @@
 (function() {
+  var __indexOf = Array.prototype.indexOf || function(item) {
+    for (var i = 0, l = this.length; i < l; i++) {
+      if (this[i] === item) return i;
+    }
+    return -1;
+  };
   $(function() {
-    var log, nav;
+    var endEvent, log, moveEvent, nav, startEvent, touchSupport;
     nav = $('#slidenav');
+    touchSupport = (__indexOf.call(window, 'ontouchstart') >= 0) || window.DocumentTouch && document instanceof DocumentTouch;
+    startEvent = touchSupport ? 'touchstart' : 'mousedown';
+    endEvent = touchSupport ? 'touchend touchcancel touchleave' : 'mouseup mouseout';
+    moveEvent = touchSupport ? 'touchmove' : 'mousemove';
     log = function(msg) {
       return $('#log').prepend("<li>" + msg + "</li>");
     };
-    nav.bind('touchstart', function(e) {
-      return $(this).addClass('touched');
+    nav.bind(startEvent, function(e) {
+      nav.addClass('touched');
+      return log(e.type);
     });
-    return nav.bind('touchend touchcancel touchleave', function(e) {
-      return $(this).removeClass('touched');
+    nav.bind(endEvent, function(e) {
+      if (!nav.is('.touched')) {
+        return;
+      }
+      nav.removeClass('touched');
+      return log(e.type);
+    });
+    return nav.bind(moveEvent, function(e) {
+      if (!nav.is('.touched')) {
+        return;
+      }
+      return log(e.type);
     });
   });
 }).call(this);
