7a7ea36c3ea6c7924beb1f7dabe9b332e4c2a507
Philipp Bosch <hello@pb.io>
2011-12-17 01:17:43 +0100

    Update app based on 85ad2688f9ea5510b96ce70b3d5cc7377e398d27

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,21 +1,15 @@
 (function() {
-  var __indexOf = Array.prototype.indexOf || function(item) {
-    for (var i = 0, l = this.length; i < l; i++) {
-      if (this[i] === item) return i;
-    }
-    return -1;
-  };
   $(function() {
     var endEvent, log, moveEvent, nav, startEvent, touchSupport;
     nav = $('#slidenav');
-    touchSupport = !!((__indexOf.call(window, 'ontouchstart') >= 0) || window.DocumentTouch && document instanceof DocumentTouch);
+    touchSupport = !!(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch);
     startEvent = touchSupport ? 'touchstart' : 'mousedown';
     endEvent = touchSupport ? 'touchend touchcancel touchleave' : 'mouseup mouseout';
     moveEvent = touchSupport ? 'touchmove' : 'mousemove';
-    alert(touchSupport);
     log = function(msg) {
       return $('#log').prepend("<li>" + msg + "</li>");
     };
+    log(touchSupport ? 'touch support' : 'no touch support');
     nav.bind(startEvent, function(e) {
       nav.addClass('touched');
       return log(e.type);
